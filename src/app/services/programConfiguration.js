70d72d4f4a7050426894e9cfdaa805c9a5e7b966
Venkateshwaran Selvaraj <venkateshwarans@ilimi.in>
2019-11-14 15:28:19 +0530

    update config

--- src/app/services/programConfiguration.js
+++ src/app/services/programConfiguration.js
@@ -1,196 +1,292 @@
 {
-  'headerComponent': {
-    'tabs': [{
-        'index': 0,
-        'label': 'Contribute Questions',
-        'onClick': {
-          'component': 'collectionComponent'
+  "headerComponent": {
+    "tabs": [
+      {
+        "index": 0,
+        "label": "Contribute Questions",
+        "onClick": {
+          "component": "collectionComponent"
         },
-        'actionRoleMap': {
-          'show': {
-            'roles': [
-              'Contributor',
-              'Reviewer'
+        "actionRoleMap": {
+          "show": {
+            "roles": [
+              "Contributor",
+              "Reviewer"
             ]
           },
-          'drag': {
-            'roles': [
-              'Contributor',
-              'Reviewer'
+          "drag": {
+            "roles": [
+              "Contributor",
+              "Reviewer"
             ]
           }
         }
       },
       {
-        'index': 1,
-        'label': 'Issue Certificate',
-        'onClick': {
-          'component': 'issueCertificateComponent'
+        "index": 1,
+        "label": "Issue Certificate",
+        "onClick": {
+          "component": "issueCertificateComponent"
         },
-        'actionRoleMap': {
-          'show': {
-            'roles': [
-              'Reviewer'
+        "actionRoleMap": {
+          "show": {
+            "roles": [
+              "Reviewer"
             ]
           }
         }
       },
       {
-        'index': 2,
-        'label': 'Dashboard',
-        'onClick': {
-          'component': 'dashboardComponent'
+        "index": 2,
+        "label": "Dashboard",
+        "onClick": {
+          "component": "dashboardComponent"
         },
-        'actionRoleMap': {
-          'show': {
-            'action': 'show',
-            'roles': [
-              'Admin'
+        "actionRoleMap": {
+          "show": {
+            "roles": [
+              "Admin"
             ]
           }
         }
       }
     ]
   },
-  'uploadComponent': {
-    'actionRoleMap': {
-      'show': {
-        'action': 'show',
-        'roles': [
-          'Contributor'
+  "collectionComponent": {
+    "actionRoleMap": {
+      "show": {
+        "roles": [
+          "Admin",
+          "Reviewer",
+          "Contributor"
         ]
       },
-      'selectFiles': {
-        'action': 'Select Files',
-        'roles': [
-          'Contributor'
-        ]
-      },
-      'upload': {
-        'action': 'upload',
-        'roles': [
-          'Contributor'
-        ]
-      },
-      'reject': {
-        'action': 'reject',
-        'roles': [
-          'Reviewer'
-        ]
-      },
-      'accept': {
-        'action': 'accept',
-        'roles': [
-          'Reviewer'
-        ]
-      },
-      'submit': {
-        'action': 'submit',
-        'roles': [
-          'Contributor'
-        ]
-      },
-      'changeFile': {
-        'action': 'changeFile',
-        'roles': [
-          'Contributor'
+      "filters": {
+        "type": [
+          "class",
+          "subject"
+        ],
+        "roles": [
+          "Admin",
+          "Reviewer",
+          "Contributor"
         ]
       }
     }
   },
-  'previewComponent': {
-    'actionRoleMap': {
-      'show': {
-        'action': 'show',
-        'roles': [
-          'Contributor', 'Reviewer'
+  "chapterlistComponent": {
+    "actionRoleMap": {
+      "addresource": {
+        "roles": [
+          "Contributor"
         ]
       }
+    },
+    "preview": {
+      "roles": [
+        "Contributor",
+        "Reviewer"
+      ]
+    },
+    "move": {
+      "roles": [
+        "Contributor"
+      ]
+    },
+    "delete": {
+      "roles": [
+        "Contributor"
+      ]
+    },
+    "selectChapter": {
+      "roles": [
+        "Contributor",
+        "Reviewer"
+      ]
+    },
+    "showTotalContribution": {
+      "lable": "Total Contributoion",
+      "roles": [
+        "Contributor",
+        "Reviewer"
+      ]
+    },
+    "showMyContribution": {
+      "lable": "My Contribution",
+      "roles": [
+        "Contributor"
+      ]
+    },
+    "showRejected": {
+      "lable": "Rejected",
+      "roles": [
+        "Contributor"
+      ]
+    },
+    "showUnderReview": {
+      "lable": "Under Review",
+      "roles": [
+        "Contributor"
+      ]
+    },
+    "showtotalUnderReview": {
+      "lable": "Total under review",
+      "roles": [
+        "Reviewer"
+      ]
+    },
+    "showAcceptedByMe": {
+      "lable": "Rejected by me",
+      "roles": [
+        "Reviewer"
+      ]
+    },
+    "showRejectedByMe": {
+      "lable": "Accepted by me",
+      "roles": [
+        "Reviewer"
+      ]
     }
   },
-  'collectionComponent': {
-    'actionRoleMap': {
-      'show': {
-        'roles': [
-          'Admin', 'Reviewer', 'Contributor'
+  "PracticeQuestionSetComponent": {
+    "No of options": 4,
+    "solutionType": [
+      "Video",
+      "Text & image"
+    ],
+    "questionCategory": [
+      "vsa",
+      "sa",
+      "ls",
+      "mcq",
+      "curiosity"
+    ],
+    "formConfiguration": [
+      {
+        "code": "LearningOutcome",
+        "range": [],
+        "label": "Learning Outcome",
+        "multiselect": false
+      },
+      {
+        "code": "bloomslevel",
+        "range": [],
+        "label": "Learning Level",
+        "multiselect": true
+      }
+    ],
+    "actionRoleMap": {
+      "save": {
+        "roles": [
+          "Contributor"
         ]
       },
-      'filters': {
-        'type': ['class', 'subject'],
-        'roles': [
-          'Admin', 'Reviewer', 'Contributor'
+      "preview": {
+        "roles": [
+          "Contributor",
+          "Reviewer"
+        ]
+      },
+      "submit": {
+        "roles": [
+          "Contributor"
+        ]
+      },
+      "delete": {
+        "roles": [
+          "Contributor"
+        ]
+      },
+      "accept": {
+        "roles": [
+          "Reviewer"
+        ]
+      },
+      "reject": {
+        "roles": [
+          "Reviewer"
+        ]
+      },
+      "addQuestion": {
+        "roles": [
+          "Contributor"
+        ]
+      },
+      "addSolution": {
+        "roles": [
+          "Contributor"
         ]
       }
     }
   },
-  'chapterlistComponent': {
-    'actionRoleMap': {
-      'addresource: {
-      'roles': [
-        'Contributor'
-      ]
-    },
-    'preview': {
-      'roles': [
-        'Contributor', 'Reviewer'
-      ]
-    },
-    'move': {
-      'roles': [
-        'Contributor'
-      ]
-    },
-    'delete': {
-      'roles': [
-        'Contributor'
-      ]
-    },
-    'selectChapter': {
-      'roles': [
-        'Contributor', 'Reviewer'
-      ]
-    },
-    'showTotalContribution': {
-      'lable': 'Total Contributoion',
-      'roles': [
-        'Contributor', 'Reviewer'
-      ]
-    },
-    'showMyContribution': {
-      'lable': 'My Contribution',
-      'roles': [
-        'Contributor'
-      ]
-    },
-    'showRejected': {
-      'lable': 'Rejected',
-      'roles': [
-        'Contributor'
-      ]
-    },
-    'showUnderReview': {
-      'lable': 'Under Review',
-      'roles': [
-        'Contributor'
-      ]
-    },
-    'showtotalUnderReview': {
-      'lable': 'Total under review',
-      'roles': [
-        'Reviewer'
-      ]
-    },
-    'showAcceptedByMe': {
-      'lable': 'Rejected by me',
-      'roles': [
-        'Reviewer'
-      ]
-    },
-    'showRejectedByMe': {
-      'lable': 'Accepted by me',
-      'roles': [
-        'Reviewer'
-      ]
+  "uploadComponent": {
+    "actionRoleMap": {
+      "show": {
+        "roles": [
+          "Contributor"
+        ]
+      },
+      "selectFiles": {
+        "roles": [
+          "Contributor"
+        ]
+      },
+      "upload": {
+        "roles": [
+          "Contributor"
+        ]
+      },
+      "reject": {
+        "roles": [
+          "Reviewer"
+        ]
+      },
+      "accept": {
+        "roles": [
+          "Reviewer"
+        ]
+      },
+      "submit": {
+        "roles": [
+          "Contributor"
+        ]
+      },
+      "changeFile": {
+        "roles": [
+          "Contributor"
+        ]
+      }
+    }
+  },
+  "previewComponent": {
+    "actionRoleMap": {
+      "show": {
+        "roles": [
+          "Contributor",
+          "Reviewer"
+        ]
+      }
+    }
+  },
+  "dashboardComponent": {
+    "actionRoleMap": {
+      "show": {
+        "roles": [
+          "Admin"
+        ]
+      },
+      "reportSelection": {
+        "label": "Select Report"
+      },
+      "contentTypeSelection": {
+        "label": "Select Content-Type"
+      },
+      "download": {
+        "label": "Download"
+      },
+      "refresh": {
+        "label": "Refresh"
+      }
     }
   }
 }
